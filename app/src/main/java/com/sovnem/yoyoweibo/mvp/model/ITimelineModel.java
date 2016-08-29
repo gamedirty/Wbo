eea64e8a0e35ff08d693abbf1b3b58d539ea02ba
gamedirty <gamedirty@hotmail.com>
2016-08-29 23:54:31 +0800

    继续重构个

--- app/src/main/java/com/sovnem/yoyoweibo/mvp/model/ITimelineModel.java
+++ app/src/main/java/com/sovnem/yoyoweibo/mvp/model/ITimelineModel.java
@@ -1,7 +1,12 @@
 package com.sovnem.yoyoweibo.mvp.model;
 
+import com.gamedirty.bean.Status;
+
+import java.util.Collection;
+
 /**
  * Created by sovnem on 16/8/29,23:14.
  */
 public interface ITimelineModel {
+    Collection<Status> getStatus();
 }
